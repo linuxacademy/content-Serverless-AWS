ded10f23dba38dc2961903c465219c279d41c43d
Moosa Khalid <moosa.khalid@linuxacademy.com>
2019-09-27 17:40:07 -0400

    added lambda handler to section 5 function

--- Section5-BackendLayer/rds-data-api.py
+++ Section5-BackendLayer/rds-data-api.py
@@ -2,19 +2,19 @@ import boto3
 
 rds_client = boto3.client('rds-data')
 database_name = 'information_schema'
-db_cluster_arn = '<DB-CLUSTER-ARN>'
-db_credentials_secret = '<SECRETS-MANAGER-SECRET-ARN>'
+db_cluster_arn = 'arn:aws:rds:us-east-1:987963314345:cluster:moosakhalid'
+db_credentials_secret = 'arn:aws:secretsmanager:us-east-1:987963314345:secret:rds-db-credentials/cluster-H3CA5N243BR2URRMFJUOPKYYP4/admin-zF6lff'
 
-def execute_statement(sql):
-    response = rds_client.execute_statement(
+def lambda_handler(event,context):
+    def execute_statement(sql):
+        response = rds_client.execute_statement(
             database = database_name,
             resourceArn = db_cluster_arn,
             secretArn = db_credentials_secret,
             sql = sql
             )
-    return response
-
-response = execute_statement(f'create database if not exists moosa')
-#print(response['records']) Un-comment this if you're runnig a select query and expect rows
-print(response)
+        return response
+    response = execute_statement(f'create database if not exists moosa')
+    #print(response['records']) Un-comment this if you're runnig a select query and expect rows
+    print(response)
 
